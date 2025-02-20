c01c57afa56eb7886bf6e5e2cb7b28e33bc20639
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2025-02-20 08:59:02 +0000

    h

--- scripts/fewshot_spp.sh
+++ scripts/fewshot_spp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -lex
 # SLURM SUBMIT SCRIPT
 #SBATCH --job-name=slice
-#SBATCH --output=/lustre/scratch/client/movian/research/users/hainn14/PointCLIP/sbatch/slurm_%A.log
-#SBATCH --error=/lustre/scratch/client/movian/research/users/hainn14/PointCLIP/sbatch/slurm_%A.err
+#SBATCH --output=/lustre/scratch/client/movian/research/users/hainn14/PointCLIP/slurm_%A.log
+#SBATCH --error=/lustre/scratch/client/movian/research/users/hainn14/PointCLIP/slurm_%A.err
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1
