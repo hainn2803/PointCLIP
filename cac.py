bee5da9d039cfcfdd17c778bc72602cbf5798c47
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2025-02-20 08:54:55 +0000

    h

--- cac.py
+++ cac.py
@@ -2,6 +2,8 @@ import torch
 import time
 from trainers.sinkhorn import SinkhornAlgorithm
 from torch.nn import functional as F
+import ot
+
 # for i in range(100):
 
 #     x = torch.randn(100, 50, 2048).cuda()
@@ -53,19 +55,25 @@ for i in range(10):
 
 
 
-    def Sinkhorn(K, u, v, max_iter):
-        r = torch.ones_like(u)
-        c = torch.ones_like(v)
-        for i in range(max_iter):
-            r0 = r
-            r = u / torch.matmul(K, c.unsqueeze(-1)).squeeze(-1)
-            c = v / torch.matmul(K.permute(0, 2, 1).contiguous(), r.unsqueeze(-1)).squeeze(-1)
-            err = (r - r0).abs().mean()
-            if err.item() < thresh:
-                break
-        T = torch.matmul(r.unsqueeze(-1), c.unsqueeze(-2)) * K
-        return T
+    # def Sinkhorn(K, u, v, max_iter):
+    #     r = torch.ones_like(u)
+    #     c = torch.ones_like(v)
+    #     for i in range(max_iter):
+    #         r0 = r
+    #         r = u / torch.matmul(K, c.unsqueeze(-1)).squeeze(-1)
+    #         c = v / torch.matmul(K.permute(0, 2, 1).contiguous(), r.unsqueeze(-1)).squeeze(-1)
+    #         err = (r - r0).abs().mean()
+    #         if err.item() < thresh:
+    #             break
+    #     T = torch.matmul(r.unsqueeze(-1), c.unsqueeze(-2)) * K
+    #     return T
+    # T_hat = Sinkhorn(wdist, p, q, max_iter)
+    # print(torch.mean(T - T_hat))
 
-    T_hat = Sinkhorn(wdist, p, q, max_iter)
-
-    print(torch.mean(T - T_hat))
+    total_error = 0
+    for i in range(len(p)):
+        T_pot = ot.sinkhorn(a=p[i], b=q[i], M=wdist[i], reg=eps, numItermax=max_iter, stopThr=thresh, method="sinkhorn_log")
+        # print(T_pot)
+        total_error += torch.mean(T[i] - T_pot)
+    
+    print(total_error)
---
+++ scripts/fewshot_spp.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -lex
+# SLURM SUBMIT SCRIPT
+#SBATCH --job-name=hplaneX
+#SBATCH --output=/lustre/scratch/client/movian/research/users/hainn14/PointCLIP/sbatch/slurm_%A.log
+#SBATCH --error=/lustre/scratch/client/movian/research/users/hainn14/PointCLIP/sbatch/slurm_%A.err
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-gpu=50GB
+#SBATCH --partition=research
+#SBATCH --mail-type=all
+#SBATCH --mail-user=v.hainn14@vinai.io
+
+# (optional) debugging flags
+# export NCCL_DEBUG=INFO
+# export PYTHONFAULTHANDLER=1
+# export NCCL_SOCKET_IFNAME=bond0
+
+   
+module purge
+module load python/miniconda3/miniconda3
+eval "$(conda shell.bash hook)"
+
+conda deactivate
+conda activate /lustre/scratch/client/movian/research/users/hainn14/envs/pc
+
+#!/bin/bash
+cd ..
+
+# Path to dataset
+DATA=data/modelnet40_ply_hdf5_2048
+DATASET=modelnet40
+
+# PointCLIP_ZS or PointCLIP_FS
+TRAINER=PointCLIP_FS
+# Trainer configs: rn50, rn101, vit_b32 or vit_b16
+CFG=rn101
+
+CTP=end  # class token position (end or middle)
+NCTX=16  # number of context tokens
+# SHOTS=$5  # number of shots (1, 2, 4, 8, 16)
+CSC=False  # class-specific context (False or True)
+NUM_PROMPTS=4  # number of proxy
+LOGIT_SCALE=on # on off
+
+# Shot number
+NUM_SHOTS=16
+
+export CUDA_VISIBLE_DEVICES=0,1,2,4,5
+python train.py \
+--root ${DATA} \
+--trainer ${TRAINER} \
+--num-shots ${NUM_SHOTS} \
+--dataset-config-file configs/datasets/${DATASET}.yaml \
+--config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+--output-dir output_spp/PROMPT_LR_0.08/ADAPTER_LR_0.01/NUM_SHOTS_${NUM_SHOTS}/NUM_PROMPTS_${NUM_PROMPTS}/PLOT_learnable_prompt_logit_scale_${LOGIT_SCALE}/${TRAINER}/${CFG}/${DATASET} \
+--num-prompts ${NUM_PROMPTS} \
+--logit-scale ${LOGIT_SCALE} \
+--post-search
